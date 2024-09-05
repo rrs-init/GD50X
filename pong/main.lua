7dac59030629c19bc8d3a13eec7f21a94f2bb936
Ryan <ryan@createcode.dev>
2024-09-05 04:21:15 -0600

    add paddles

--- pong/main.lua
+++ pong/main.lua
@@ -9,6 +9,8 @@ VIRTUAL_HEIGHT = 243
 
 function love.load()
 	love.graphics.setDefaultFilter("nearest", "nearest");
+	smallFont = love.graphics.newFont('font.ttf', 8);
+	love.graphics.setFont(smallFont);
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
 		fullscreen = false,
 		resizable = false,
@@ -18,8 +20,10 @@ end
 
 function love.draw()
 	push:apply('start')
-
+	love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
 	love.graphics.printf('Hello', 0, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center');
+	love.graphics.rectangle("fill", 10, 30, 5, 20)
+	love.graphics.rectangle("fill", VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20);
 
 
 
@@ -28,4 +32,10 @@ end
 
 function love.update(dt)
 
+end
+
+function love.keypressed(key)
+	if key == 'escape' then
+		love.event.quit()
+	end
 end
