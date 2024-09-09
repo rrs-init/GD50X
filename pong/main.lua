07453a4dc05574d50c7ef76223144c8dc5db6e75
Ryan <ryan@createcode.dev>
2024-09-09 00:17:22 -0600

    add final sounds

--- pong/main.lua
+++ pong/main.lua
@@ -21,6 +21,12 @@ function love.load()
 	player1 = Paddle(10, 30, 5, 20)
 	player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
 	ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+	sounds = {
+		['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+		['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+		['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+	}
 	Push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
 		fullscreen = false,
 		resizable = false,
@@ -75,6 +81,7 @@ function love.update(dt)
 			else
 				ball.dy = math.random(10, 150)
 			end
+			sounds['paddle_hit']:play()
 		end
 		if ball:collides(player2) then
 			ball.dx = -ball.dx * 1.03
@@ -84,14 +91,17 @@ function love.update(dt)
 			else
 				ball.dy = math.random(10, 150)
 			end
+			sounds['paddle_hit']:play()
 		end
 		if ball.y <= 0 then
 			ball.y = 0
 			ball.dy = -ball.dy
+			sounds['wall_hit']:play()
 		end
 		if ball.y >= VIRTUAL_HEIGHT - 4 then
 			ball.y = VIRTUAL_HEIGHT - 4
 			ball.dy = -ball.dy
+			sounds['wall_hit']:play()
 		end
 	end
 	-- Player 1 Movement
@@ -117,6 +127,7 @@ function love.update(dt)
 	if ball.x < 0 then
 		servingPlayer = 1
 		P2Score = P2Score + 1
+		sounds['score']:play()
 		if P2Score == 10 then
 			winningPlayer = 2
 			gameState = 'done'
@@ -129,7 +140,7 @@ function love.update(dt)
 	if ball.x > VIRTUAL_WIDTH then
 		servingPlayer = 2
 		P1Score = P1Score + 1
-
+		sounds['score']:play()
 		if P1Score == 10 then
 			winningPlayer = 1
 			gameState = 'done'
